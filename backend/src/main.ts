d4c6b5945b6a1e77e54f25a1e1f99831b5a668d0
Angel Araujo <angelxdrodrigo@gmail.com>
2026-05-26 13:52:29 -0700

    Solucion a la petcicion hacia el backend para el login

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,11 +12,13 @@ async function bootstrap() {
   });
 
   // Configuración de validaciones automáticas
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true, // Borra datos que no estén en el DTO
-    forbidNonWhitelisted: true, // Lanza error si mandan datos extra
-    transform: true, // Transforma tipos automáticamente
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Borra datos que no estén en el DTO
+      forbidNonWhitelisted: true, // Lanza error si mandan datos extra
+      transform: true, // Transforma tipos automáticamente
+    }),
+  );
 
   app.enableCors(); // <-- Para que React pueda comunicarse con el Backend
 
